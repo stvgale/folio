b4cc4e465ee878db9fc0360ad44be4f99156edb4
stevegale <stvgale@gmail.com>
2019-06-26 15:27:35 -0700

    Update IndexPagePreview.js

--- src/cms/preview-templates/IndexPagePreview.js
+++ src/cms/preview-templates/IndexPagePreview.js
@@ -11,6 +11,8 @@ const IndexPagePreview = ({ entry, getAsset }) => {
         title={data.title}
         heading={data.heading}
         description={data.description}
+        intro={data.intro || { blurbs: [] }}
+        mainpitch={data.mainpitch || {}}
       />
     )
   } else {
