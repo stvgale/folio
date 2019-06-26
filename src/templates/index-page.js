4843ab398152ce777aef8aedfe97aa302b8d6349
stevegale <stvgale@gmail.com>
2019-06-26 15:27:37 -0700

    Update index-page.js

--- src/templates/index-page.js
+++ src/templates/index-page.js
@@ -8,6 +8,9 @@ import BlogRoll from '../components/BlogRoll'
 
 export const IndexPageTemplate = ({
   title,
+  heading,
+  mainpitch,
+  description,
   intro,
 }) => (
   <div>
@@ -30,7 +33,12 @@ export const IndexPageTemplate = ({
             <div className="column is-10 is-offset-1">
               <div className="content">
                 <div className="content">
-                  
+                  <div className="tile">
+                    <h1 className="title">{mainpitch.title}</h1>
+                  </div>
+                  <div className="tile">
+                    <h3 className="subtitle">{mainpitch.description}</h3>
+                  </div>
                 </div>
                 <div className="columns">
                   <div className="column is-12">
@@ -40,7 +48,25 @@ export const IndexPageTemplate = ({
                     <p>{description}</p>
                   </div>
                 </div>
-                
+                <Features gridItems={intro.blurbs} />
+                <div className="columns">
+                  <div className="column is-12 has-text-centered">
+                    <Link className="btn" to="/products">
+                      See all products
+                    </Link>
+                  </div>
+                </div>
+                <div className="column is-12">
+                  <h3 className="has-text-weight-semibold is-size-2">
+                    Latest stories
+                  </h3>
+                  <BlogRoll />
+                  <div className="column is-12 has-text-centered">
+                    <Link className="btn" to="/blog">
+                      Read more
+                    </Link>
+                  </div>
+                </div>
               </div>
             </div>
           </div>
@@ -52,6 +78,12 @@ export const IndexPageTemplate = ({
 
 IndexPageTemplate.propTypes = {
   title: PropTypes.string,
+  heading: PropTypes.string,
+  mainpitch: PropTypes.object,
+  description: PropTypes.string,
+  intro: PropTypes.shape({
+    blurbs: PropTypes.array,
+  }),
 }
 
 const IndexPage = ({ data }) => {
@@ -61,6 +93,9 @@ const IndexPage = ({ data }) => {
     <Layout>
       <IndexPageTemplate
         title={frontmatter.title}
+        heading={frontmatter.heading}
+        mainpitch={frontmatter.mainpitch}
+        description={frontmatter.description}
         intro={frontmatter.intro}
       />
     </Layout>
@@ -82,9 +117,16 @@ export const pageQuery = graphql`
     markdownRemark(frontmatter: { templateKey: { eq: "index-page" } }) {
       frontmatter {
         title
+        heading
+        mainpitch {
+          title
+          description
+        }
         description
         intro {
-        ]
+          blurbs {
+            text
+          }
           heading
           description
         }
