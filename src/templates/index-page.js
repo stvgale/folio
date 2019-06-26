4517036de96717f9e908440a26fd751c4529c6ac
stevegale <stvgale@gmail.com>
2019-06-26 15:33:49 -0700

    Update index-page.js

--- src/templates/index-page.js
+++ src/templates/index-page.js
@@ -48,14 +48,7 @@ export const IndexPageTemplate = ({
                     <p>{description}</p>
                   </div>
                 </div>
-                <Features gridItems={intro.blurbs} />
-                <div className="columns">
-                  <div className="column is-12 has-text-centered">
-                    <Link className="btn" to="/products">
-                      See all products
-                    </Link>
-                  </div>
-                </div>
+       
               </div>
             </div>
           </div>
