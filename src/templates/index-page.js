e127ff365a02a31970ef49c41d516e6c961666bf
stevegale <stvgale@gmail.com>
2019-06-26 15:55:49 -0700

    Update index-page.js

--- src/templates/index-page.js
+++ src/templates/index-page.js
@@ -1,18 +1,25 @@
 import React from 'react'
 import PropTypes from 'prop-types'
-import { Link, graphql } from 'gatsby'
-
+import { graphql } from 'gatsby'
 import Layout from '../components/Layout'
 import Features from '../components/Features'
-
+import Testimonials from '../components/Testimonials'
+import Pricing from '../components/Pricing'
 import PreviewCompatibleImage from '../components/PreviewCompatibleImage'
 
-export const IndexPageTemplate = ({
+export const ProductPageTemplate = ({
+  image,
   title,
-    main,
+  heading,
+  description,
+  intro,
+  main,
+  testimonials,
+  fullImage,
+  pricing,
 }) => (
-  <div>
-        <h1
+  <div className="content">
+   <h1
           className="has-text-weight-bold is-size-3-mobile is-size-2-tablet is-size-1-widescreen home-title"
           style={{
             lineHeight: '1',
@@ -28,46 +35,59 @@ export const IndexPageTemplate = ({
       <div className="container">
         <div className="section">
           <div className="columns">
+            <div className="column is-7 is-offset-1">
+              <h3 className="has-text-weight-semibold is-size-2">{heading}</h3>
+              <p>{description}</p>
+            </div>
+          </div>
+          <div className="columns">
             <div className="column is-10 is-offset-1">
-              <div className="content">
-                <div className="content">
-                  <div className="tile">
-                   
-                  </div>
-                  <div className="tile">
-                   <h3 className="has-text-weight-semibold is-size-3">
+              <Features gridItems={intro.blurbs} />
+              <div className="columns">
+                <div className="column is-7">
+                  <h3 className="has-text-weight-semibold is-size-3">
                     {main.heading}
                   </h3>
                   <p>{main.description}</p>
-    <article className="tile is-child">
+                </div>
+              </div>
+              <div className="tile is-ancestor">
+                <div className="tile is-vertical">
+                  <div className="tile">
+                    <div className="tile is-parent is-vertical">
+                      <article className="tile is-child">
                         <PreviewCompatibleImage imageInfo={main.image1} />
                       </article>
-    <article className="tile is-child">
+                    </div>
+                    <div className="tile is-parent">
+                      <article className="tile is-child">
                         <PreviewCompatibleImage imageInfo={main.image2} />
                       </article>
-    <article className="tile is-child">
-                        <PreviewCompatibleImage imageInfo={main.image3} />
-                      </article>
+                    </div>
                   </div>
-                </div>
-                <div className="columns">
-                  <div className="column is-12">
-                
-                  </div>
-                </div>
-               
-                <div className="columns">
-                  <div className="column is-12 has-text-centered">
-                   
-                  </div>
-                </div>
-                <div className="column is-12">
-
-                  <div className="column is-12 has-text-centered">
-                   
+                  <div className="tile is-parent">
+                    <article className="tile is-child">
+                      <PreviewCompatibleImage imageInfo={main.image3} />
+                    </article>
                   </div>
                 </div>
               </div>
+              <Testimonials testimonials={testimonials} />
+              <div
+                className="full-width-image-container"
+                style={{
+                  backgroundImage: `url(${
+                    fullImage.childImageSharp
+                      ? fullImage.childImageSharp.fluid.src
+                      : fullImage
+                  })`,
+                }}
+              />
+              <h2 className="has-text-weight-semibold is-size-2">
+                {pricing.heading}
+              </h2>
+              <p className="is-size-5">{pricing.description}</p>
+              <Pricing data={pricing.plans} />
             </div>
           </div>
         </div>
@@ -76,33 +96,51 @@ export const IndexPageTemplate = ({
   </div>
 )
 
-IndexPageTemplate.propTypes = {
+ProductPageTemplate.propTypes = {
+  image: PropTypes.oneOfType([PropTypes.object, PropTypes.string]),
   title: PropTypes.string,
-      main: PropTypes.shape({
+  heading: PropTypes.string,
+  description: PropTypes.string,
+  intro: PropTypes.shape({
+    blurbs: PropTypes.array,
+  }),
+  main: PropTypes.shape({
     heading: PropTypes.string,
     description: PropTypes.string,
     image1: PropTypes.oneOfType([PropTypes.object, PropTypes.string]),
     image2: PropTypes.oneOfType([PropTypes.object, PropTypes.string]),
     image3: PropTypes.oneOfType([PropTypes.object, PropTypes.string]),
   }),
-
+  testimonials: PropTypes.array,
+  fullImage: PropTypes.oneOfType([PropTypes.object, PropTypes.string]),
+  pricing: PropTypes.shape({
+    heading: PropTypes.string,
+    description: PropTypes.string,
+    plans: PropTypes.array,
+  }),
 }
 
-const IndexPage = ({ data }) => {
+const ProductPage = ({ data }) => {
   const { frontmatter } = data.markdownRemark
 
   return (
     <Layout>
-      <IndexPageTemplate
+      <ProductPageTemplate
+        image={frontmatter.image}
         title={frontmatter.title}
-       main={frontmatter.main}
-       
+        heading={frontmatter.heading}
+        description={frontmatter.description}
+        intro={frontmatter.intro}
+        main={frontmatter.main}
+        testimonials={frontmatter.testimonials}
+        fullImage={frontmatter.full_image}
+        pricing={frontmatter.pricing}
       />
     </Layout>
   )
 }
 
-IndexPage.propTypes = {
+ProductPage.propTypes = {
   data: PropTypes.shape({
     markdownRemark: PropTypes.shape({
       frontmatter: PropTypes.object,
@@ -110,15 +148,37 @@ IndexPage.propTypes = {
   }),
 }
 
-export default IndexPage
+export default ProductPage
 
-export const pageQuery = graphql`
-  query IndexPageTemplate {
-    markdownRemark(frontmatter: { templateKey: { eq: "index-page" } }) {
+export const productPageQuery = graphql`
+  query ProductPage($id: String!) {
+    markdownRemark(id: { eq: $id }) {
       frontmatter {
         title
-      }
-main {
+        image {
+          childImageSharp {
+            fluid(maxWidth: 2048, quality: 100) {
+              ...GatsbyImageSharpFluid
+            }
+          }
+        }
+        heading
+        description
+        intro {
+          blurbs {
+            image {
+              childImageSharp {
+                fluid(maxWidth: 240, quality: 64) {
+                  ...GatsbyImageSharpFluid
+                }
+              }
+            }
+            text
+          }
+          heading
+          description
+        }
+        main {
           heading
           description
           image1 {
@@ -152,6 +212,28 @@ main {
             }
           }
         }
+        testimonials {
+          author
+          quote
+        }
+        full_image {
+          childImageSharp {
+            fluid(maxWidth: 2048, quality: 100) {
+              ...GatsbyImageSharpFluid
+            }
+          }
+        }
+        pricing {
+          heading
+          description
+          plans {
+            description
+            items
+            plan
+            price
+          }
+        }
+      }
     }
   }
 `
