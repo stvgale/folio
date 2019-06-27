d0b8db7dab72dac89f2cd2d2a36610c3d4601fe4
stevegale <stvgale@gmail.com>
2019-06-26 20:10:06 -0700

    Update Footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -18,26 +18,7 @@ const Footer = class extends React.Component {
               <div className="column is-4">
                 <section className="menu">
                   <ul className="menu-list">
-                    <li>
-                      <Link to="/" className="navbar-item">
-                        Home
-                      </Link>
-                    </li>
-                    <li>
-                      <Link className="navbar-item" to="/about">
-                        About
-                      </Link>
-                    </li>
-                    <li>
-                      <Link className="navbar-item" to="/products">
-                        Products
-                      </Link>
-                    </li>
-                    <li>
-                      <Link className="navbar-item" to="/contact/examples">
-                        Form Examples
-                      </Link>
-                    </li>
+                    
                     <li>
                       <a
                         className="navbar-item"
