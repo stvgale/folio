e49e8b00d85c5b2cf1cb6768e758738e0b5ec454
stevegale <stvgale@gmail.com>
2019-06-26 17:23:47 -0700

    Update index-page.js

--- src/templates/index-page.js
+++ src/templates/index-page.js
@@ -2,6 +2,7 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import { graphql } from 'gatsby'
 import Layout from '../components/Layout'
+import Plx from 'react-plx';
 
 
 import PreviewCompatibleImage from '../components/PreviewCompatibleImage'
