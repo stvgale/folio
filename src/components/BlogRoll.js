2b0699881218db0f5dba42250dc4e8255d777770
stevegale <stvgale@gmail.com>
2019-06-27 21:03:35 -0700

    images

--- src/components/BlogRoll.js
+++ src/components/BlogRoll.js
@@ -106,7 +106,7 @@ export default () => (
                 featuredpost
                 featuredimage {
                   childImageSharp {
-                    fluid(maxWidth: 120, quality: 100) {
+                    fluid(maxWidth: 420, quality: 100) {
                       ...GatsbyImageSharpFluid
                     }
                   }
